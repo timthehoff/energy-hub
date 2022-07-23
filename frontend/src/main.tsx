4d9397b463d28a5b459f7e61e5ab2cd8da5f9ced
Chris Hoff <chriscasola@gmail.com>
2022-07-23 17:01:24 -0400

    Start on login page (#4)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 
-ReactDOM.render(
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
