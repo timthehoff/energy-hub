cbc6cf07cbd89340316b5f32214c98cf981f1344
Tim Hoff <thoff@wayfair.com>
2022-07-30 14:56:30 -0400

    Charging endpoints

--- backend/app/vehicle.py
+++ backend/app/vehicle.py
@@ -1,3 +1,4 @@
+from asyncio import sleep
 from typing import Dict
 
 from fastapi import APIRouter, Depends, HTTPException
@@ -6,6 +7,9 @@ from teslapy import Tesla
 
 from app import get_tesla
 from app.data.config import VehicleConfig
+
+WAKE_SLEEP = 2
+WAKE_TIMEOUT = 60
 
 router = APIRouter(prefix="/vehicles")
 
@@ -17,6 +21,35 @@ async def get_config() -> VehicleConfig:
         raise HTTPException(
             status.HTTP_400_BAD_REQUEST, detail="Vehicle not configured"
         )
+
+
+async def is_awake(tesla: Tesla, vehicle_id: int) -> bool:
+    response = tesla.api("VEHICLE_SUMMARY", path_vars={"vehicle_id": vehicle_id})
+    return response["response"]["state"] == "online"
+
+
+async def wake_and_run(
+    tesla: Tesla, vehicle_id: int, command: str, params: dict = None
+) -> dict:
+
+    # Wake up.
+    for _ in range(WAKE_TIMEOUT // WAKE_SLEEP):
+        if await is_awake(tesla, vehicle_id):
+            break
+
+        tesla.api("WAKE_UP", path_vars={"vehicle_id": vehicle_id})
+        await sleep(2)
+
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_408_REQUEST_TIMEOUT,
+            detail="Unable to wake vehicle",
+        )
+
+    # Run command.
+    params = params or {}
+    response: dict = tesla.api(command, path_vars={"vehicle_id": vehicle_id}, **params)
+    return response.get("response") or response
 
 
 @router.get("", tags=["Vehicles"])
@@ -51,3 +84,39 @@ async def get_status(
 ) -> dict:
     response: dict = tesla.api("VEHICLE_DATA", path_vars={"vehicle_id": config.id})
     return response["response"]
+
+
+@router.post("/charge/start", tags=["Vehicles", "Charging"])
+async def start_charging(
+    tesla: Tesla = Depends(get_tesla), config: VehicleConfig = Depends(get_config)
+):
+    return await wake_and_run(tesla, config.id, "START_CHARGE")
+
+
+@router.post("/charge/stop", tags=["Vehicles", "Charging"])
+async def stop_charging(
+    tesla: Tesla = Depends(get_tesla), config: VehicleConfig = Depends(get_config)
+):
+    return await wake_and_run(tesla, config.id, "STOP_CHARGE")
+
+
+@router.post("/charge/limit", tags=["Vehicles", "Charging"])
+async def set_charge_limit(
+    percent: int,
+    tesla: Tesla = Depends(get_tesla),
+    config: VehicleConfig = Depends(get_config),
+):
+    return await wake_and_run(
+        tesla, config.id, "CHANGE_CHARGE_LIMIT", params={"percent": percent}
+    )
+
+
+@router.post("/charge/amps", tags=["Vehicles", "Charging"])
+async def set_charge_amps(
+    amps: int,
+    tesla: Tesla = Depends(get_tesla),
+    config: VehicleConfig = Depends(get_config),
+):
+    return await wake_and_run(
+        tesla, config.id, "CHARGING_AMPS", params={"charging_amps": amps}
+    )
